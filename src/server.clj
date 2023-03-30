7faea4c2af8cfb49513d54038e6e1aeb1a2722aa
Dmitry Shutov <dmitry.s@beda.software>
2023-03-30 10:37:15 +0700

    Add routing

--- src/server.clj
+++ src/server.clj
@@ -1,9 +1,27 @@
 (ns server
-  (:require [org.httpkit.server :as server]))
+  (:require
+   [org.httpkit.server :as server]
+   [compojure.route :as route]
+   [compojure.core :refer [defroutes, POST]]
+   ;[com.health-samurai/jute :as jute]
+   ))
 
-(defn app [req]
-  {:status  200
-   :headers {"Content-Type" "text/html"}
-   :body    "hello HTTP!"})
+(defn parse-jute-template [req]
+  {:status 200
+   :headers {"Content-Type" "application/json"}
+   :body {:test "Test text"}})
 
-(server/run-server app {:port 8090})
+(defroutes all-routes
+  (POST "/parse-template" [] parse-jute-template)
+  (route/not-found "<p>Page not found.</p>"))
+
+(defn run
+  "Run the web server"
+  []
+  (let [port (Integer/parseInt (or (System/getenv "APP_PORT") "8090"))]
+    (server/run-server all-routes {:port port})
+    (println (str "Runnning webserver at 0.0.0.0:" port))))
+
+(defn -main
+  [& _args]
+  (run))
