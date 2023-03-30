a9376df7d4c05eb585298f1afafab73aa60ad9b7
Dmitry Shutov <dmitry.s@beda.software>
2023-03-30 12:46:07 +0700

    Fix JUTE dependency namespace

--- src/server.clj
+++ src/server.clj
@@ -3,7 +3,7 @@
    [org.httpkit.server :as http-server]
    [compojure.route :as route]
    [compojure.core :refer [defroutes, POST]]
-   ;[com.health-samurai/jute :as jute]
+   [jute.core :as jute]
    ))
 
 (defonce server (atom nil))
@@ -14,21 +14,26 @@
     (reset! server nil)))
 
 (defn parse-jute-template [req]
-  {:status 200
-   :headers {"Content-Type" "application/json"}
-   :body {:test "Test text"}})
+
+  (let [resources (:body req)]
+    (println "Template: " (get resources "template"))
+    {:status 200
+     :headers {"Content-Type" "application/json"}
+     :body {"returned-template" (get resources "template" "test")}}))
 
 (defroutes all-routes
   (POST "/parse-template" [] parse-jute-template)
   (route/not-found "<p>Page not found.</p>"))
 
-(defn run
-  "Run the web server"
+(defn run-server
   []
   (let [port (Integer/parseInt (or (System/getenv "APP_PORT") "8090"))]
     (reset! server (http-server/run-server all-routes {:port port}))
     (println (str "Runnning webserver at 0.0.0.0:" port))))
 
+;(run-server)
+;(stop-server)
+
 (defn -main
   [& _args]
-  (run))
+  (run-server))
